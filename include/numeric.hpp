21f594ba1401f891369f438879caa4355480f5d1
hexu <hexu_1985@sina.com>
2019-10-31 17:43:54 +0800

    add comments for gcd

--- include/numeric.hpp
+++ include/numeric.hpp
@@ -4,12 +4,16 @@
 namespace mini_math {
 
 /**
- * @brief 计算x^y mode N
+ * @brief 模的指数运算: 计算x^y mode N
  *
- * @tparam Integer 整数
+ * @tparam Integer 整数类型
  * @param x 底数
  * @param y 指数
  * @param N 模数
+ *
+ * @warning: x, y, N必须是正整数
+ *
+ * @see 算法概论中文版: 章节1.2.2
  */
 template <typename Integer>
 Integer modexp(Integer x, Integer y, Integer N)
@@ -23,6 +27,19 @@ Integer modexp(Integer x, Integer y, Integer N)
         return (x*z*z) % N;
 }
 
+/**
+ * @brief 欧几里得的最大公因数算法
+ *
+ * @tparam Integer 整数类型
+ * @param a 整数a
+ * @param b 整数b
+ *
+ * @return (a, b): a和b的最大公因数
+ *
+ * @warning: a, b必须是正整数, 且a>b
+ *
+ * @see 算法概论中文版: 章节1.2.3
+ */
 template <typename Integer>
 Integer gcd_euclid(Integer a, Integer b)
 {
@@ -30,6 +47,17 @@ Integer gcd_euclid(Integer a, Integer b)
     return gcd_euclid(b, a%b);
 }
 
+/**
+ * @brief 计算最大公因数
+ *
+ * @tparam Integer 整数类型
+ * @param a 整数a
+ * @param b 整数b
+ *
+ * @return (a, b): a和b的最大公因数
+ *
+ * @warning: a, b必须是正整数
+ */
 template <typename Integer>
 Integer gcd(Integer a, Integer b)
 {
