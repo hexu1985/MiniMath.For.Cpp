2f7d17eebd14f226b7598a253a9aed248cb7d423
hexu <hexu_1985@sina.com>
2019-11-04 16:55:08 +0800

    add comments and doxygen file

--- include/numeric.hpp
+++ include/numeric.hpp
@@ -1,3 +1,10 @@
+/**
+ * @file numeric.hpp
+ * @brief 数论相关函数
+ * @author hexu_1985@sina.com
+ * @version 1.0
+ * @date 2019-11-04
+ */
 #ifndef MINI_MATH_NUMERIC_INC
 #define MINI_MATH_NUMERIC_INC
 
@@ -6,7 +13,7 @@
 namespace mini_math {
 
 /**
- * @brief 模的指数运算: 计算x^y mode N
+ * @brief 模的指数运算: 计算\f$x^y mod N\f$
  *
  * @tparam Integer 整数类型
  * @param x 底数
@@ -69,6 +76,21 @@ Integer gcd(Integer a, Integer b)
         return gcd_euclid(a, b);
 }
 
+/**
+ * @brief Euclid算法的一种扩展: 用线性组合的方式来表示最大公因子: \n
+ *          即如果x, y, d = gcd_euclid_ext(a, b), 那么d = (a, b), \n
+ *          并且\f$d = a \times x+b \times y\f$
+ *
+ * @tparam Integer 整数类型
+ * @param a 整数a
+ * @param b 整数b
+ *
+ * @return x, y, d, 其中d=(a, b), 并且\f$d = a \times x+b \times y\f$
+ *
+ * @warning: a, b必须是正整数, 且a>b
+ *
+ * @see 算法概论中文版: 章节1.2.4
+ */
 template <typename Integer>
 std::tuple<Integer, Integer, Integer> gcd_euclid_ext(Integer a, Integer b)
 {
@@ -78,6 +100,21 @@ std::tuple<Integer, Integer, Integer> gcd_euclid_ext(Integer a, Integer b)
     return std::make_tuple(y, x - (a/b)*y, d);
 }
 
+/**
+ * @brief Euclid算法的一种扩展: 用线性组合的方式来表示最大公因子: \n
+ *          即如果d = gcd(a, b, x, y), 那么d = (a, b), \n
+ *          并且\f$d = a \times x+b \times y\f$
+ *
+ * @tparam Integer 整数类型
+ * @param a 整数a
+ * @param b 整数b
+ * @param [out] x 整数x
+ * @param [out] y 整数y
+ *
+ * @return (a, b): a和b的最大公因数, 并且\f$d = a \times x+b \times y\f$
+ *
+ * @warning: a, b必须是正整数
+ */
 template <typename Integer>
 Integer gcd(Integer a, Integer b, Integer &x, Integer &y)
 {
